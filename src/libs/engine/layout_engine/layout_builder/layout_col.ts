6fd4b455df509d873a738e2d32d70cfe9a45c429
Diorbert Pereira <diorbert@gmail.com>
2022-05-10 20:39:36 -0300

    Layout row, Layout col creation

---
+++ src/libs/engine/layout_engine/layout_builder/layout_col.ts
@@ -0,0 +1,180 @@
+import {Alignment} from "@libs/common/Alignment.enum";
+import {Settings} from "@libs/engine/layout_engine/settings";
+import {BaseElement} from "@libs/model/base_element";
+import {LayoutElementGroup} from "@libs/engine/layout_engine/layout_builder/layout_element_group";
+
+export class LayoutCol extends LayoutElementGroup {
+    constructor(
+        mainAxisAlignment: Alignment,
+        crossAxisAlignment: Alignment,
+        settings: Settings,
+        parentId: string,
+        withoutMargin: boolean
+    ) {
+        super(mainAxisAlignment, crossAxisAlignment, settings, parentId, withoutMargin);
+    }
+
+    getWidth() {
+        if (this.children.length > 0) {
+            if (this.withoutMargin) {
+                return this.crossLength;
+            } else {
+                return this.crossLength + 2 * this.settings.marginY;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    getHeight() {
+        if (this.children.length > 0) {
+            if (this.withoutMargin) {
+                return this.virtualMainLength;
+            } else {
+                return this.virtualMainLength + 2 * this.settings.marginX;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    setUsefulWidth(value: number, extraWidth: number) {
+        const currentWidth = this.getWidth();
+
+        if (value > currentWidth) {
+            const totalMargin = this.withoutMargin ? 0 : 2 * this.settings.marginX;
+
+            if (value > totalMargin) {
+                this.setMaximumCrossLength(value - totalMargin - extraWidth);
+            } else {
+                throw new Error("Empty useful area");
+            }
+        } else if (value < currentWidth) {
+            throw new Error("The new Width can´t be smaller than current Width");
+        }
+    }
+
+    setUsefulHeight(value: number, extraHeight: number) {
+        const currentHeight = this.getHeight();
+
+        if (value > currentHeight) {
+            const totalMargin = this.withoutMargin ? 0 : 2 * this.settings.marginY;
+
+            if (value > totalMargin) {
+                this.setMaximumMainLength(value - totalMargin - extraHeight);
+            } else {
+                throw new Error("Empty useful area");
+            }
+        } else if (value < currentHeight) {
+            throw new Error("The new Height can´t be smaller than current Height");
+        }
+    }
+
+    setWidth(value: number) {
+        this.setUsefulWidth(value, 0);
+    }
+
+    setHeight(value: number) {
+        this.setUsefulHeight(value, 0);
+    }
+
+    addContainer(container: BaseElement | LayoutElementGroup) {
+        if (container) {
+            super.addContainer(container);
+
+            if (this.children.length > 1) {
+                super.incrementMainLength(this.getOptimalPadding() + container.getHeight());
+            } else {
+                super.incrementMainLength(container.getHeight());
+            }
+
+            super.setMaximumCrossLength(container.getWidth());
+
+            if (container instanceof BaseElement) {
+                this.applyMainAxisDistribution();
+                this.applyCrossAxisDistribution();
+            }
+        }
+    }
+
+    adjustDimensionsToChildren() {
+        super.adjustDimensionsToChildren(
+            (child: BaseElement | LayoutElementGroup) => child.getHeight(),
+            (child: BaseElement | LayoutElementGroup) => child.getWidth()
+        );
+    }
+
+    setMaximumMainLength(value: number) {
+        super.setMaximumMainLength(value);
+
+        this.applyMainAxisDistribution();
+    }
+
+    setMaximumCrossLength(value: number) {
+        super.setMaximumCrossLength(value);
+
+        this.applyCrossAxisDistribution();
+    }
+
+    /**
+     * Distributes children over the element area, considering Main Axis and Cross Axis alignment options
+     */
+    applyDistribution() {
+        this.applyMainAxisDistribution();
+        this.applyCrossAxisDistribution();
+    }
+
+    /**
+     * Distributes children over the element area, considering Main Axis alignment option
+     */
+    applyMainAxisDistribution() {
+        const refSize = super.getOptimalSize();
+        const refPadding = super.getOptimalPadding();
+        let refPosition = super.getOptimalStartPosition(); // Setting the initial cursor position
+
+        // Adjusting size and position for all children
+        for (let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            const mainSize = child.getHeight();
+
+            if (this.mainAxisAlignment === Alignment.EXPANDED) {
+                child.setHeight(refSize);
+                child.setY(refPosition);
+
+                refPosition += refSize + refPadding;
+            } else if (this.mainAxisAlignment === Alignment.START || this.mainAxisAlignment === Alignment.CENTER) {
+                child.setY(refPosition);
+
+                refPosition += mainSize + refPadding;
+            } else if (this.mainAxisAlignment === Alignment.END) {
+                refPosition -= mainSize;
+
+                child.setY(refPosition);
+
+                refPosition -= refPadding;
+            }
+        }
+    }
+
+    /**
+     * Distributes children over the element area, considering Cross Axis alignment option
+     */
+    applyCrossAxisDistribution() {
+        // Adjusting size and position for all children
+        for (let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            const mainSize = child.getWidth();
+
+            if (this.crossAxisAlignment === Alignment.EXPANDED) {
+                child.setWidth(super.getCrossLength());
+                child.setX(0);
+            } else if (this.crossAxisAlignment === Alignment.START) {
+                child.setX(0);
+            } else if (this.crossAxisAlignment === Alignment.CENTER) {
+                child.setX(super.getCrossLength() / 2 - mainSize / 2);
+            } else if (this.crossAxisAlignment === Alignment.END) {
+                child.setX(super.getCrossLength() - mainSize);
+            }
+        }
+    }
+}
---
+++ src/libs/engine/layout_engine/layout_builder/layout_row.ts
@@ -0,0 +1,171 @@
+import {Alignment} from "@libs/common/Alignment.enum";
+import {Settings} from "@libs/engine/layout_engine/settings";
+import {LayoutElementGroup} from "@libs/engine/layout_engine/layout_builder/layout_element_group";
+import {BaseElement} from "@libs/model/base_element";
+
+export class LayoutRow extends LayoutElementGroup {
+    constructor(
+        mainAxisAlignment: Alignment,
+        crossAxisAlignment: Alignment,
+        settings: Settings,
+        parentId: string,
+        withoutMargin: boolean
+    ) {
+        super(mainAxisAlignment, crossAxisAlignment, settings, parentId, withoutMargin);
+    }
+
+    getWidth() {
+        if (this.children.length > 0) {
+            if (this.withoutMargin) {
+                return this.virtualMainLength;
+            } else {
+                return this.virtualMainLength + 2 * this.settings.marginX;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    getHeight() {
+        if (this.children.length > 0) {
+            if (this.withoutMargin) {
+                return this.crossLength;
+            } else {
+                return this.crossLength + 2 * this.settings.marginY;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    setUsefulWidth(value: number, extraWidth: number) {
+        const currentWidth = this.getWidth();
+
+        if (value > currentWidth) {
+            const totalMargin = this.withoutMargin ? 0 : 2 * this.settings.marginX;
+
+            if (value > totalMargin) {
+                this.setMaximumMainLength(value - totalMargin - extraWidth);
+            } else {
+                throw new Error("Empty useful area");
+            }
+        } else if (value < currentWidth) {
+            throw new Error("The new Width can´t be smaller than current Width");
+        }
+    }
+
+    setUsefulHeight(value: number, extraHeight: number) {
+        const currentHeight = this.getHeight();
+
+        if (value > currentHeight) {
+            const totalMargin = this.withoutMargin ? 0 : 2 * this.settings.marginY;
+
+            if (value > totalMargin) {
+                this.setMaximumCrossLength(value - totalMargin - extraHeight);
+            } else {
+                throw new Error("Empty useful area");
+            }
+        } else if (value < currentHeight) {
+            throw new Error("The new Height can´t be smaller than current Height");
+        }
+    }
+
+    setWidth(value: number) {
+        this.setUsefulWidth(value, 0);
+    }
+
+    setHeight(value: number) {
+        this.setUsefulHeight(value, 0);
+    }
+
+    addContainer(container: BaseElement | LayoutElementGroup) {
+        if (container) {
+            super.addContainer(container);
+
+            if (this.children.length > 1) {
+                super.incrementMainLength(this.getOptimalPadding() + container.getWidth());
+            } else {
+                super.incrementMainLength(container.getWidth());
+            }
+
+            super.setMaximumCrossLength(container.getHeight());
+
+            if (container instanceof BaseElement) {
+                this.applyMainAxisDistribution();
+                this.applyCrossAxisDistribution();
+            }
+        }
+    }
+
+    adjustDimensionsToChildren() {
+        super.adjustDimensionsToChildren(
+            (child: BaseElement | LayoutElementGroup) => child.getWidth(),
+            (child: BaseElement | LayoutElementGroup) => child.getHeight()
+        );
+    }
+
+    setMaximumMainLength(value: number) {
+        super.setMaximumMainLength(value);
+
+        this.applyMainAxisDistribution();
+    }
+
+    setMaximumCrossLength(value: number) {
+        super.setMaximumCrossLength(value);
+
+        this.applyCrossAxisDistribution();
+    }
+
+    applyDistribution() {
+        this.applyMainAxisDistribution();
+        this.applyCrossAxisDistribution();
+    }
+
+    applyMainAxisDistribution() {
+        const refSize = super.getOptimalSize();
+        const refPadding = super.getOptimalPadding();
+        let refPosition = super.getOptimalStartPosition(); // Setting the initial cursor position
+
+        // Adjusting size and position for all children
+        for (let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            const mainSize = child.getWidth();
+
+            if (this.mainAxisAlignment === Alignment.EXPANDED) {
+                child.setWidth(refSize);
+                child.setX(refPosition);
+
+                refPosition += refSize + refPadding;
+            } else if (this.mainAxisAlignment === Alignment.START || this.mainAxisAlignment === Alignment.CENTER) {
+                child.setX(refPosition);
+
+                refPosition += mainSize + refPadding;
+            } else if (this.mainAxisAlignment === Alignment.END) {
+                refPosition -= mainSize;
+
+                child.setX(refPosition);
+
+                refPosition -= refPadding;
+            }
+        }
+    }
+
+    applyCrossAxisDistribution() {
+        // Adjusting size and position for all children
+        for (let i = 0; i < this.children.length; i++) {
+            const child = this.children[i];
+            const mainSize = child.getHeight();
+
+            if (this.crossAxisAlignment === Alignment.EXPANDED) {
+                child.setHeight(super.getCrossLength());
+                child.setY(0);
+            } else if (this.crossAxisAlignment === Alignment.START) {
+                child.setY(0);
+            } else if (this.crossAxisAlignment === Alignment.CENTER) {
+                child.setY(super.getCrossLength() / 2 - mainSize / 2);
+            } else if (this.crossAxisAlignment === Alignment.END) {
+                child.setY(super.getCrossLength() - mainSize);
+            }
+        }
+    }
+}
