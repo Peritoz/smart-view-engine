e247a9ee227b99e3a1f976875f661ce6aa96b2e4
Diorbert Pereira <diorbert@gmail.com>
2022-05-21 19:43:13 -0300

    WIP: Reimplementing distribution functions

--- src/libs/engine/layout_engine/layout_builder/layout_element_group.ts
+++ src/libs/engine/layout_engine/layout_builder/layout_element_group.ts
@@ -16,7 +16,7 @@ export class LayoutElementGroup {
   protected settings: Settings;
   protected horizontalAlignment: Alignment;
   protected verticalAlignment: Alignment;
-  protected children: any[];
+  protected children: Array<LayoutElementGroup | BaseElement>;
   protected x: number;
   protected y: number;
   protected childrenDirection: Direction;
@@ -81,12 +81,314 @@ export class LayoutElementGroup {
     }
   }
 
-  addContainer(container: object) {
+  getUsedWidth() {
+    return this.usedWidth;
+  }
+
+  getUsedHeight() {
+    return this.usedHeight;
+  }
+
+  getChildrenLength() {
+    return this.children.length;
+  }
+
+  getChildren() {
+    return this.children;
+  }
+
+  setX(value: number) {
+    this.x = value;
+  }
+
+  setY(value: number) {
+    this.y = value;
+  }
+
+  getX() {
+    return this.x;
+  }
+
+  getY() {
+    return this.y;
+  }
+
+  getWidth(): number {
+    return this.width;
+  }
+
+  getHeight(): number {
+    return this.height;
+  }
+
+  setWidth(value: number) {
+    if (value > this.width) {
+      this.width = value;
+
+      // Updating content box limit
+      this.updateHorizontalContentBoxAxis();
+
+      // Updating reference element size
+      this.updateSizeReference();
+    } else {
+      throw new Error("The new Width can´t be smaller than current Width");
+    }
+  }
+
+  setHeight(value: number) {
+    if (value > this.height) {
+      this.height = value;
+
+      // Updating content box limit
+      this.updateVerticalContentBoxAxis();
+
+      // Updating reference element size
+      this.updateSizeReference();
+    } else {
+      throw new Error("The new Height can´t be smaller than current Height");
+    }
+  }
+
+  updateHorizontalContentBoxAxis() {
+    this.contentBox.bottomRight.x = this.width - this.settings.rightPadding;
+  }
+
+  updateVerticalContentBoxAxis() {
+    this.contentBox.bottomRight.y = this.height - this.settings.bottomPadding;
+  }
+
+  /**
+   * Based on alignment and offset, returns the optimal the initial position for nested children
+   * @returns Initial position
+   */
+  getInitialNestedPosition(
+    virtualLength: number,
+    usedLength: number,
+    alignment: Alignment,
+    offset: number = 0
+  ): number {
+    if (alignment === Alignment.END) {
+      return virtualLength;
+    } else if (alignment === Alignment.CENTER) {
+      const centerPoint = (virtualLength - offset) / 2 + offset;
+      return centerPoint - usedLength / 2;
+    } else {
+      // START or EXPANDED with padding
+      return offset;
+    }
+  }
+
+  resetElementLength() {
+    this.usedWidth = 0;
+    this.usedHeight = 0;
+  }
+
+  incrementUsedWidth(value: number) {
+    this.usedWidth += value;
+
+    if (this.usedWidth > this.width) {
+      this.width = this.usedWidth;
+    }
+
+    // Updating maximum element width
+    if (value > this.maxChildWidth) {
+      this.maxChildWidth = value;
+    }
+
+    this.updateSizeReference();
+
+    // Updating content box limit
+    this.updateHorizontalContentBoxAxis();
+  }
+
+  incrementUsedHeight(value: number) {
+    this.usedHeight += value;
+
+    if (this.usedHeight > this.height) {
+      this.height = this.usedHeight;
+    }
+
+    // Updating maximum element height
+    if (value > this.maxChildHeight) {
+      this.maxChildHeight = value;
+    }
+
+    this.updateSizeReference();
+
+    // Updating content box limit
+    this.updateVerticalContentBoxAxis();
+  }
+
+  updateSizeReference() {
+    const virtualLengthWithoutPadding =
+      this.width - (this.children.length - 1) * this.settings.spaceBetween;
+    const potentialOptimalSize =
+      virtualLengthWithoutPadding / this.children.length;
+
+    if (potentialOptimalSize <= this.maxChildWidth && this.hasNestedGroup) {
+      this.sizeReference = this.maxChildWidth;
+    } else {
+      this.sizeReference = potentialOptimalSize;
+    }
+  }
+
+  addContainer(container: BaseElement | LayoutElementGroup) {
+    const isHorizontal = this.childrenDirection === Direction.HORIZONTAL;
+    const incrementUsedMainLength: (value: number) => void = isHorizontal
+      ? this.incrementUsedWidth
+      : this.incrementUsedHeight;
+    const setCrossDimension: (value: number) => void = isHorizontal
+      ? this.setWidth
+      : this.setHeight;
+    let mainIncrementValue: number = isHorizontal
+      ? container.getWidth()
+      : container.getHeight();
+    const crossIncrementValue: number = isHorizontal
+      ? container.getHeight()
+      : container.getWidth();
+
     if (container) {
       this.children.push(container);
 
       this.hasNestedGroup =
         this.hasNestedGroup || container instanceof LayoutElementGroup;
+
+      if (this.children.length > 1) {
+        mainIncrementValue += this.settings.spaceBetween;
+      }
+
+      incrementUsedMainLength(mainIncrementValue);
+      setCrossDimension(crossIncrementValue);
+
+      if (container instanceof BaseElement) {
+        this.applyDistribution();
+        container.setParentId(this.id);
+      }
+    }
+  }
+
+  applyDistribution(): void {
+    this.applyHorizontalDistribution();
+    this.applyVerticalDistribution();
+  }
+
+  /**
+   * Distributes children over the element area, considering Main Axis alignment option
+   */
+  applyHorizontalDistribution() {
+    this.distributeElements(
+      this.verticalAlignment,
+      this.getWidth(),
+      (child) => child.getWidth(),
+      (child, value) => child.setWidth(value),
+      (child, value) => child.setX(value),
+      this.verticalAlignment === Alignment.END
+        ? this.contentBox.bottomRight.y
+        : this.contentBox.topLeft.y
+    );
+  }
+
+  /**
+   * Distributes children over the element area, considering Cross Axis alignment option
+   */
+  applyVerticalDistribution() {
+    this.alignElements(
+      this.verticalAlignment,
+      this.getHeight(),
+      (child) => child.getHeight(),
+      (child, value) => child.setY(value),
+      this.verticalAlignment === Alignment.END
+        ? this.contentBox.bottomRight.y
+        : this.contentBox.topLeft.y
+    );
+  }
+
+  /**
+   * Distributes children over the element area, considering alignment option
+   * @param alignment Alignment option to be applied
+   * @param totalSize Container dimension length to be considered
+   * @param getChildSize Callback to get the child dimension length
+   * @param setChildSize Callback to set the child dimension length
+   * @param setChildPosition Callback to set the child position
+   * @param offset Offset to be considered when calculating position
+   */
+  distributeElements(
+    alignment: Alignment,
+    totalSize: number,
+    getChildSize: (child: LayoutElementGroup | BaseElement) => number,
+    setChildSize: (
+      child: LayoutElementGroup | BaseElement,
+      value: number
+    ) => void,
+    setChildPosition: (
+      child: LayoutElementGroup | BaseElement,
+      value: number
+    ) => void,
+    offset: number = 0
+  ) {
+    const refSize = this.sizeReference;
+    const spaceBetween = this.settings.spaceBetween;
+    let cursor = offset; // Setting the initial cursor position
+
+    // Adjusting size and position for all children
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+
+      if (alignment === Alignment.EXPANDED) {
+        setChildSize(child, refSize);
+        setChildPosition(child, cursor);
+
+        cursor += refSize + spaceBetween;
+      } else if (
+        alignment === Alignment.START ||
+        alignment === Alignment.CENTER
+      ) {
+        setChildPosition(child, cursor);
+
+        cursor += totalSize + spaceBetween;
+      } else if (alignment === Alignment.END) {
+        cursor -= totalSize;
+
+        setChildPosition(child, cursor);
+
+        cursor -= spaceBetween;
+      }
+    }
+  }
+
+  /**
+   * Aligns children over the element area, considering alignment option
+   * @param alignment Alignment option to be applied
+   * @param totalSize Container dimension length to be considered
+   * @param getChildSize Callback to get the child dimension length
+   * @param setChildPosition Callback to set the child position
+   * @param offset Offset to be considered when calculating position
+   */
+  alignElements(
+    alignment: Alignment,
+    totalSize: number,
+    getChildSize: (child: LayoutElementGroup | BaseElement) => number,
+    setChildPosition: (
+      child: LayoutElementGroup | BaseElement,
+      value: number
+    ) => void,
+    offset: number = 0
+  ) {
+    // Adjusting size and position for all children
+    for (let i = 0; i < this.children.length; i++) {
+      const child = this.children[i];
+      const childSize = getChildSize(child);
+
+      if (alignment === Alignment.EXPANDED) {
+        child.setHeight(totalSize);
+        setChildPosition(child, offset);
+      } else if (alignment === Alignment.START) {
+        setChildPosition(child, offset);
+      } else if (alignment === Alignment.CENTER) {
+        setChildPosition(child, totalSize / 2 - childSize / 2);
+      } else if (alignment === Alignment.END) {
+        setChildPosition(child, totalSize - offset - childSize);
+      }
     }
   }
 
@@ -165,181 +467,4 @@ export class LayoutElementGroup {
       child.translatePosition(this.getX(), this.getY());
     }
   }
-
-  setX(value: number) {
-    this.x = value;
-  }
-
-  setY(value: number) {
-    this.y = value;
-  }
-
-  getX() {
-    return this.x;
-  }
-
-  getY() {
-    return this.y;
-  }
-
-  updateHorizontalContentBoxAxis() {
-    this.contentBox.bottomRight.x = this.width - this.settings.rightPadding;
-  }
-
-  updateVerticalContentBoxAxis() {
-    this.contentBox.bottomRight.y = this.height - this.settings.bottomPadding;
-  }
-
-  /**
-   * Based on alignment and offset, returns the optimal the initial position for nested children
-   * @returns Initial position
-   */
-  getInitialNestedPosition(
-    virtualLength: number,
-    usedLength: number,
-    alignment: Alignment,
-    offset: number = 0
-  ): number {
-    if (alignment === Alignment.END) {
-      return virtualLength;
-    } else if (alignment === Alignment.CENTER) {
-      const centerPoint = (virtualLength - offset) / 2 + offset;
-      return centerPoint - usedLength / 2;
-    } else {
-      // START or EXPANDED with padding
-      return offset;
-    }
-  }
-
-  resetElementLength() {
-    this.usedWidth = 0;
-    this.usedHeight = 0;
-  }
-
-  incrementUsedWidth(value: number) {
-    this.usedWidth += value;
-
-    if (this.usedWidth > this.width) {
-      this.width = this.usedWidth;
-    }
-
-    // Updating maximum element width
-    if (value > this.maxChildWidth) {
-      this.maxChildWidth = value;
-    }
-
-    this.updateSizeReference();
-
-    // Updating content box limit
-    this.updateHorizontalContentBoxAxis();
-  }
-
-  incrementUsedHeight(value: number) {
-    this.usedHeight += value;
-
-    if (this.usedHeight > this.height) {
-      this.height = this.usedHeight;
-    }
-
-    // Updating maximum element height
-    if (value > this.maxChildHeight) {
-      this.maxChildHeight = value;
-    }
-
-    this.updateSizeReference();
-
-    // Updating content box limit
-    this.updateVerticalContentBoxAxis();
-  }
-
-  setMaximumCrossLength(value: number) {
-    if (value > this.usedHeight) {
-      this.height = value;
-
-      this.updateSizeReference();
-    }
-  }
-
-  setMaximumMainLength(value: number) {
-    if (value > this.usedWidth) {
-      this.width = value;
-
-      this.updateSizeReference();
-    }
-  }
-
-  updateSizeReference() {
-    const virtualLengthWithoutPadding =
-      this.width - (this.children.length - 1) * this.getOptimalPadding();
-    const potentialOptimalSize =
-      virtualLengthWithoutPadding / this.children.length;
-
-    if (potentialOptimalSize <= this.maxChildWidth && this.hasNestedGroup) {
-      this.sizeReference = this.maxChildWidth;
-    } else {
-      this.sizeReference = potentialOptimalSize;
-    }
-  }
-
-  getMainLength(virtual: boolean) {
-    return virtual ? this.width : this.usedWidth;
-  }
-
-  getCrossLength() {
-    return this.usedHeight;
-  }
-
-  getChildrenLength() {
-    return this.children.length;
-  }
-
-  getChildren() {
-    return this.children;
-  }
-
-  /**
-   * Based on alignment, returns the optimal element size to fulfill the main axis in a balanced manner
-   * @returns {number}
-   */
-  getOptimalSize() {
-    return this.sizeReference;
-  }
-
-  getOptimalPadding() {
-    return this.settings.spaceBetween;
-  }
-
-  // TODO: Calculate total width
-  getWidth(): number {
-    return 0;
-  }
-
-  // TODO: Calculate total height
-  getHeight(): number {
-    return 0;
-  }
-
-  setWidth(value: number) {
-    if (value > this.width) {
-      this.width = value;
-
-      // Updating content box limit
-      this.updateHorizontalContentBoxAxis();
-    } else {
-      throw new Error("The new Width can´t be smaller than current Width");
-    }
-  }
-
-  setHeight(value: number) {
-    if (value > this.height) {
-      this.height = value;
-
-      // Updating content box limit
-      this.updateVerticalContentBoxAxis();
-    } else {
-      throw new Error("The new Height can´t be smaller than current Height");
-    }
-  }
-
-  applyDistribution(): void {}
 }
