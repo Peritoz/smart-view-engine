10ded5a25dd99d72c8bd5be3c86eb79d53dd54e5
Diorbert Pereira <diorbert@gmail.com>
2022-06-10 19:41:26 -0300

    Updating mixed layout tests

--- __tests__/unit/block_layout/mixed_layout.test.ts
+++ __tests__/unit/block_layout/mixed_layout.test.ts
@@ -23,18 +23,18 @@ describe("Mixed Rendering", () => {
 
   it("Horizontal - Row Resizing - After Insert Cols", async () => {
     let row1 = new LayoutRow(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
     let col1 = new LayoutCol(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
     let col2 = new LayoutCol(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
 
@@ -67,27 +67,27 @@ describe("Mixed Rendering", () => {
     const col1Children = cols[0].getChildren();
     const col2Children = cols[1].getChildren();
 
-    expect(row1.getHeight()).toBe(100);
-    expect(row1.getWidth()).toBe(120);
-    expect(col1Children[0].getWidth()).toBe(60);
+    expect(row1.getHeight()).toBe(80);
+    expect(row1.getWidth()).toBe(90);
+    expect(col1Children[0].getWidth()).toBe(50);
     expect(col1Children[1].getWidth()).toBe(60);
-    expect(col2Children[0].getWidth()).toBe(60);
+    expect(col2Children[0].getWidth()).toBe(25);
   });
 
   it("Horizontal - Row Resizing - Imposed Size", async () => {
     let row1 = new LayoutRow(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
     let col1 = new LayoutCol(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
     let col2 = new LayoutCol(
-      Alignment.EXPANDED,
-      Alignment.EXPANDED,
+      Alignment.START,
+      Alignment.START,
       settings
     );
 
@@ -116,31 +116,31 @@ describe("Mixed Rendering", () => {
     row1.addContainer(col1);
     row1.addContainer(col2);
 
-    row1.setWidth(200);
-    row1.setHeight(125);
+    row1.setWidth(120);
+    row1.setHeight(100);
 
     const cols = row1.getChildren();
     const col1Children = cols[0].getChildren();
     const col2Children = cols[1].getChildren();
 
     expect(col1Children[0].getY()).toBe(0);
-    expect(col1Children[1].getY()).toBe(55);
+    expect(col1Children[1].getY()).toBe(30);
     expect(col2Children[0].getY()).toBe(0);
-    expect(col1Children[0].getHeight()).toBe(50);
+    expect(col1Children[0].getHeight()).toBe(25);
     expect(col1Children[1].getHeight()).toBe(50);
-    expect(col2Children[0].getHeight()).toBe(105);
-    expect(col1Children[0].getWidth()).toBe(60);
+    expect(col2Children[0].getHeight()).toBe(60);
+    expect(col1Children[0].getWidth()).toBe(50);
     expect(col1Children[1].getWidth()).toBe(60);
-    expect(col2Children[0].getWidth()).toBe(60);
-    expect(row1.getHeight()).toBe(125);
-    expect(row1.getWidth()).toBe(200);
+    expect(col2Children[0].getWidth()).toBe(25);
+    expect(row1.getWidth()).toBe(120);
+    expect(row1.getHeight()).toBe(100);
   });
 
   it("Horizontal and Vertical - Simple Resizing", async () => {
     const layoutSettings = new Settings({
       maxHorizontalCount: 4,
       maxChildHorizontalCount: 2,
-      spaceBetween: 2,
+      spaceBetween: 5,
       sizeUnit: 5,
       leftPadding: 1,
       rightPadding: 1,
@@ -153,21 +153,25 @@ describe("Mixed Rendering", () => {
 
     const director = new LayoutDirector(layoutSettings);
 
-    const row1 = director.newRow(Alignment.EXPANDED, Alignment.EXPANDED, true);
-    const col1 = director.newCol(Alignment.START, Alignment.EXPANDED, true);
+    const row1 = director.newRow(Alignment.START, Alignment.START);
+    const col1 = director.newCol(Alignment.START, Alignment.START);
     director.addMediumElementToCurrent("A", "T", false);
     director.navigateToParent();
-    const col2 = director.newCol(Alignment.START, Alignment.EXPANDED, true);
+    const col2 = director.newCol(Alignment.START, Alignment.START);
     director.addMediumElementToCurrent("A", "T", false);
 
-    expect(row1.children.length).toBe(2);
-    expect(col1.children.length).toBe(1);
-    expect(col2.children.length).toBe(1);
+    const row1Children = row1.getChildren();
+    const col1Children = col1.getChildren();
+    const col2Children = col2.getChildren();
+
+    expect(row1Children.length).toBe(2);
+    expect(col1Children.length).toBe(1);
+    expect(col2Children.length).toBe(1);
     expect(col1.getWidth()).toBe(15);
     expect(col1.getHeight()).toBe(5);
     expect(col2.getWidth()).toBe(15);
     expect(col2.getHeight()).toBe(5);
-    expect(row1.getWidth()).toBe(32);
+    expect(row1.getWidth()).toBe(35);
     expect(row1.getHeight()).toBe(5);
   });
 
@@ -175,7 +179,7 @@ describe("Mixed Rendering", () => {
     const layoutSettings = new Settings({
       maxHorizontalCount: 4,
       maxChildHorizontalCount: 2,
-      spaceBetween: 2,
+      spaceBetween: 5,
       sizeUnit: 5,
       leftPadding: 1,
       rightPadding: 1,
@@ -188,103 +192,100 @@ describe("Mixed Rendering", () => {
 
     const director = new LayoutDirector(layoutSettings);
 
-    const row1 = director.newRow(Alignment.EXPANDED, Alignment.EXPANDED, true);
-    const col1 = director.newCol(Alignment.START, Alignment.EXPANDED, true);
-    const row2 = director.newRow(Alignment.EXPANDED, Alignment.EXPANDED, true);
-    const col2 = director.newCol(Alignment.START, Alignment.EXPANDED, true);
+    const row1 = director.newRow(Alignment.START, Alignment.START);
+    const col1 = director.newCol(Alignment.START, Alignment.START);
+    const row2 = director.newRow(Alignment.START, Alignment.START);
+    const col2 = director.newCol(Alignment.START, Alignment.START);
     director.addMediumElementToCurrent("B", "T", false);
     director.navigateToParent();
     director.navigateToParent();
     director.addMediumElementToCurrent("A", "T", false);
 
-    expect(row1.children.length).toBe(1);
-    expect(col1.children.length).toBe(2);
-    expect(row2.children.length).toBe(1);
-    expect(col2.children.length).toBe(1);
+    const row1Children = row1.getChildren();
+    const col1Children = col1.getChildren();
+    const row2Children = row2.getChildren();
+    const col2Children = col2.getChildren();
+
+    expect(row1Children.length).toBe(1);
+    expect(col1Children.length).toBe(2);
+    expect(row2Children.length).toBe(1);
+    expect(col2Children.length).toBe(1);
     expect(col1.getWidth()).toBe(15);
-    expect(col1.getHeight()).toBe(12);
+    expect(col1.getHeight()).toBe(15);
     expect(row2.getWidth()).toBe(15);
     expect(row2.getHeight()).toBe(5);
     expect(col2.getWidth()).toBe(15);
     expect(col2.getHeight()).toBe(5);
     expect(row1.getWidth()).toBe(15);
-    expect(row1.getHeight()).toBe(12);
+    expect(row1.getHeight()).toBe(15);
   });
 
   it("Horizontal - Insertion Resize (Global Distribution)", async () => {
     const set = new LayoutTree(settings);
 
-    const row1 = set.newRow(Alignment.START, Alignment.EXPANDED);
-    const col1 = set.newCol(Alignment.START, Alignment.EXPANDED);
-    const row2 = set.newRow(Alignment.START, Alignment.EXPANDED);
+    const row1 = set.newRow(Alignment.START, Alignment.START);
+    const col1 = set.newCol(Alignment.START, Alignment.START);
+    const row2 = set.newRow(Alignment.START, Alignment.START);
     const el1 = elementBuilder.buildElement({
       name: "A",
-      width: 50,
-      height: 30,
-      x: 0,
-      y: 0,
+      width: 30,
+      height: 10
     });
     const el2 = elementBuilder.buildElement({
       name: "B",
-      width: 75,
-      height: 35,
-      x: 0,
-      y: 0,
+      width: 40,
+      height: 15
     });
 
     set.addToCurrentGroup(el1);
     set.addToCurrentGroup(el2);
     set.navigateToParent();
 
-    const row3 = set.newRow(Alignment.START, Alignment.EXPANDED);
+    const row3 = set.newRow(Alignment.START, Alignment.START);
     const el3 = elementBuilder.buildElement({
       name: "A",
       width: 60,
-      height: 50,
-      x: 0,
-      y: 0,
+      height: 10
     });
 
     set.addToCurrentGroup(el3);
     set.navigateToParent();
     set.navigateToParent();
 
-    const col2 = set.newCol(Alignment.START, Alignment.EXPANDED);
-    const row4 = set.newRow(Alignment.START, Alignment.EXPANDED);
+    const col2 = set.newCol(Alignment.START, Alignment.START);
+    const row4 = set.newRow(Alignment.START, Alignment.START);
     const el4 = elementBuilder.buildElement({
       name: "A",
-      width: 100,
-      height: 60,
-      x: 0,
-      y: 0,
+      width: 30,
+      height: 20
     });
 
     set.addToCurrentGroup(el4);
 
-    expect(row2.getHeight()).toBe(45);
-    expect(row2.getWidth()).toBe(140);
-    expect(row3.getHeight()).toBe(60);
-    expect(row3.getWidth()).toBe(140);
-    expect(col1.getHeight()).toBe(120);
-    expect(col1.getWidth()).toBe(150);
-    expect(row4.getHeight()).toBe(70);
-    expect(row4.getWidth()).toBe(110);
-    expect(col2.getHeight()).toBe(120);
-    expect(col2.getWidth()).toBe(120);
-    expect(row1.getHeight()).toBe(130);
-    expect(row1.getWidth()).toBe(285);
-    expect(el1.getHeight()).toBe(35);
-    expect(el1.getWidth()).toBe(50);
-    expect(el2.getHeight()).toBe(35);
-    expect(el2.getWidth()).toBe(75);
-    expect(el3.getHeight()).toBe(50);
+    expect(row2.getHeight()).toBe(15);
+    expect(row2.getWidth()).toBe(75);
+    expect(row3.getHeight()).toBe(10);
+    expect(row3.getWidth()).toBe(60);
+    expect(col1.getHeight()).toBe(30);
+    expect(col1.getWidth()).toBe(75);
+    expect(row4.getHeight()).toBe(20);
+    expect(row4.getWidth()).toBe(30);
+    expect(col2.getHeight()).toBe(20);
+    expect(col2.getWidth()).toBe(30);
+    expect(row1.getHeight()).toBe(30);
+    expect(row1.getWidth()).toBe(110);
+    expect(el1.getHeight()).toBe(10);
+    expect(el1.getWidth()).toBe(30);
+    expect(el2.getHeight()).toBe(15);
+    expect(el2.getWidth()).toBe(40);
+    expect(el3.getHeight()).toBe(10);
     expect(el3.getWidth()).toBe(60);
-    expect(el4.getHeight()).toBe(60);
-    expect(el4.getWidth()).toBe(100);
+    expect(el4.getHeight()).toBe(20);
+    expect(el4.getWidth()).toBe(30);
 
     expect(el1.getX()).toBe(0);
     expect(el1.getY()).toBe(0);
-    expect(el2.getX()).toBe(55);
+    expect(el2.getX()).toBe(35);
     expect(el2.getY()).toBe(0);
     expect(el3.getX()).toBe(0);
     expect(el3.getY()).toBe(0);
