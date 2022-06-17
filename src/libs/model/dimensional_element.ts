fde840d8d4660ca740c8c0e453faa99b7e9705ce
Diorbert Pereira <diorbert@gmail.com>
2022-06-17 20:06:06 -0300

    Removing unused DimensionalElement class

--- src/libs/model/dimensional_element.ts
+++
@@ -1,8 +0,0 @@
-import { Dimension } from "@libs/model/dimension";
-
-export interface DimensionalElement extends Dimension {
-  getWidth: () => number;
-  getHeight: () => number;
-  setWidth: (value: number) => void;
-  setHeight: (value: number) => void;
-}
